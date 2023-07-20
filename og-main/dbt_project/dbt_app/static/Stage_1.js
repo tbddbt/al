93992699ef7225688f0a3afd55917199c776d4f2
Sid G <simplysid8449@gmial.com>
2023-07-20 08:41:27 +0530

    chosen plugin trial

--- og-main/dbt_project/dbt_app/static/Stage_1.js
+++ og-main/dbt_project/dbt_app/static/Stage_1.js
@@ -9,8 +9,13 @@ function getColumnFormat_Stage_1() {
       {readOnly: true },
       {},
       {
-        editor: 'select', 
-        selectOptions: ['Option 1', 'Option 2', 'Option 3'],
+        renderer: customDropdownRenderer,
+        editor: "chosen",
+        width: 150,
+        chosenOptions: {
+            multiple: true,
+            data: ['1','2','3']
+        }
       },{},{},{type: 'date',},{},{},{},{},{type: 'date',},{},{type: 'date',},{},{},{type: 'date',},
       {
         editor: 'select', 
@@ -19,4 +24,27 @@ function getColumnFormat_Stage_1() {
     ];
     return columns;
   }
-  
+
+function customDropdownRenderer(instance, td, row, col, prop, value, cellProperties) {
+  var selectedId;
+  var optionsList = cellProperties.chosenOptions.data;
+
+  if(typeof optionsList === "undefined" || typeof optionsList.length === "undefined" || !optionsList.length) {
+      Handsontable.cellTypes.text.renderer(instance, td, row, col, prop, value, cellProperties);
+      return td;
+  }
+
+  var values = (value + "").split(",");
+  value = [];
+  for (var index = 0; index < optionsList.length; index++) {
+
+      if (values.indexOf(optionsList[index].id + "") > -1) {
+          selectedId = optionsList[index].id;
+          value.push(optionsList[index].label);
+      }
+  }
+  value = value.join(", ");
+
+  Handsontable.cellTypes.text.renderer(instance, td, row, col, prop, value, cellProperties);
+  return td;
+}
--- og-main/dbt_project/dbt_app/static/Stage_2.js
+++ og-main/dbt_project/dbt_app/static/Stage_2.js
@@ -2,7 +2,12 @@ function getColumnFormat_Stage_2() {
     var columns;
     
         columns = [
-        {readOnly: true },{type: 'date',readOnly: true },{readOnly: true },{readOnly: true },{readOnly: true },{readOnly: true },{readOnly: true },{type: 'date',},{},{},{},{},{type: 'date',},{},{},{type: 'checkbox'},
+        {readOnly: true },{type: 'date',readOnly: true },{readOnly: true },{readOnly: true },{readOnly: true },{readOnly: true },
+        { 
+            editor: 'select', 
+            selectOptions: ['Option 1', 'Option 2', 'Option 3'],
+        },
+        {type: 'date',},{},{},{},{},{type: 'date',},{},{},{type: 'checkbox'},
         ];
         return columns;
      } 
